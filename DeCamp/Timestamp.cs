5865d3a33c987ac9d996d3518eff773ba1b7b20f
manveti <manveti@users.noreply.github.com>
2018-01-06 17:23:15 -0800

    Clean up add logic in generic calendar. Implement add logic for specific calendars.

--- DeCamp/Timestamp.cs
+++ DeCamp/Timestamp.cs
@@ -75,7 +75,6 @@ namespace DeCamp {
                 amount = days;
                 unit = Interval.day;
             }
-            if (unit <= Interval.day) { amount *= 24; } // convert days to hours
             // strip off extra years at this point to avoid overflow when we convert to seconds; we'll still need to normalize later, though
             int yearLength = this.getYearLength();
             while (amount >= yearLength) {
@@ -86,10 +85,12 @@ namespace DeCamp {
                 this.year -= 1;
                 amount += yearLength;
             }
+            if (unit <= Interval.day) { amount *= 24; } // convert days to hours
             if (unit <= Interval.hour) { amount *= 60; } // convert hours to minutes
             if (unit <= Interval.minute) { amount *= 60; } // convert minutes to seconds
             this.time += amount;
-            yearLength *= 24 * 60 * 60; // convert days to seconds
+            // normalize
+            yearLength *= this.getDayLength(); // convert days to seconds
             while (this.time >= yearLength) {
                 this.year += 1;
                 this.time -= yearLength;
@@ -114,7 +115,7 @@ namespace DeCamp {
         }
 
         protected virtual int getDayOfYear() {
-            return (this.time / 86400) + 1;
+            return (this.time / this.getDayLength()) + 1;
         }
 
         protected virtual int getYearLength() {
@@ -123,6 +124,10 @@ namespace DeCamp {
                 retval += month.Item2;
             }
             return retval;
+        }
+
+        protected virtual int getDayLength() {
+            return 24 * 60 * 60;
         }
 
         protected virtual String getFuzzyTime(int hours) {
@@ -148,7 +153,38 @@ namespace DeCamp {
             this.dateFormat = "Day {0}";
         }
 
-        //override add: ditch year logic and add date logic
+        public override void add(int amount, Interval unit = Interval.second) {
+            if (unit == Interval.time) { return; } // can't add time of day
+            if (unit == Interval.year) {
+                amount *= 365; // treat year as shorthand for 365 days
+                unit = Interval.day;
+            }
+            if (unit == Interval.month) {
+                amount *= 30; // treat month as shorthand for 30 days
+                unit = Interval.day;
+            }
+            if (unit == Interval.week) {
+                amount *= 7; // treat week as shortand for 7 days
+                unit = Interval.day;
+            }
+            if (unit <= Interval.day) {
+                this.date += amount;
+                return;
+            }
+            if (unit <= Interval.hour) { amount *= 60; } // convert hours to minutes
+            if (unit <= Interval.minute) { amount *= 60; } // convert minutes to seconds
+            this.time += amount;
+            // normalize
+            int dayLength = this.getDayLength();
+            while (this.time >= dayLength) {
+                this.date += 1;
+                this.time -= dayLength;
+            }
+            while (this.time < 0) {
+                this.date -= 1;
+                this.time += dayLength;
+            }
+        }
 
         protected override int getDayOfYear() {
             return this.date;
@@ -188,7 +224,48 @@ namespace DeCamp {
             return this.days[(date.Item2 - 1) % this.days.Length];
         }
 
-        //override add: handle festivals and weeks; fix for lack of year 0 in common reckoning, so -1 goes straight to 1
+        public override void add(int amount, Interval unit = Interval.second) {
+            if (unit == Interval.year) {
+                // deal with the fact that there's no year 0 in common reckoning
+                if ((this.year < 0) && (this.year + amount >= 0)) { this.year += 1; }
+                if ((this.year > 0) && (this.year + amount <= 0)) { this.year -= 1; }
+                this.year += amount;
+                return;
+            }
+            if (unit == Interval.month) {
+                // convert months to days, then fall through to logic for day, hour, minute, and second
+                int month = this.getDate().Item1 - 1, days = 0;
+                for (; amount > 0; amount--) {
+                    if (this.months[month].Item2 <= 7) {
+                        // skip over festivals
+                        days += this.months[month].Item2;
+                        month += 1;
+                        if (month >= this.months.Length) { month = 0; } // no end-of-year festival, so we should never need this
+                    }
+                    days += this.months[month].Item2;
+                    month += 1;
+                    if (month >= this.months.Length) { month = 0; }
+                }
+                for (; amount < 0; amount++) {
+                    month -= 1;
+                    if (month < 0) { month = this.months.Length - 1; }
+                    days -= this.months[month].Item2;
+                    if (this.months[month].Item2 <= 7) {
+                        // skip over festivals
+                        month -= 1;
+                        if (month < 0) { month = this.months.Length - 1; }
+                        days -= this.months[month].Item2;
+                    }
+                }
+                amount = days;
+                unit = Interval.day;
+            }
+            if (unit == Interval.week) {
+                amount *= this.days.Length;
+                unit = Interval.day;
+            }
+            base.add(amount, unit);
+        }
     }
 
     class EberronDate : SimpleDate {
@@ -218,7 +295,17 @@ namespace DeCamp {
             return this.days[(this.getDayOfYear() - 1) % this.days.Length];
         }
 
-        //override add: handle weeks
+        public override void add(int amount, Interval unit = Interval.second) {
+            if (unit == Interval.month) {
+                amount *= 28; // all months have 28 days
+                unit = Interval.day;
+            }
+            if (unit == Interval.week) {
+                amount *= this.days.Length;
+                unit = Interval.day;
+            }
+            base.add(amount, unit);
+        }
     }
 
     class FRDate : SimpleDate {
@@ -226,24 +313,24 @@ namespace DeCamp {
             this.year = 1491;
             this.time = 12 * 60 * 60;
             this.precision = Interval.time;
-            this.months = new Tuple<String, int>[]{ new Tuple<String, int>("Hammer", 28),
+            this.months = new Tuple<String, int>[]{ new Tuple<String, int>("Hammer", 30),
                                                     new Tuple<String, int>("Midwinter", 1),
-                                                    new Tuple<String, int>("Alturiak", 28),
-                                                    new Tuple<String, int>("Ches", 28),
-                                                    new Tuple<String, int>("Tarsakh", 28),
+                                                    new Tuple<String, int>("Alturiak", 30),
+                                                    new Tuple<String, int>("Ches", 30),
+                                                    new Tuple<String, int>("Tarsakh", 30),
                                                     new Tuple<String, int>("Greengrass", 1),
-                                                    new Tuple<String, int>("Mirtul", 28),
-                                                    new Tuple<String, int>("Kythorn", 28),
-                                                    new Tuple<String, int>("Flamerule", 28),
+                                                    new Tuple<String, int>("Mirtul", 30),
+                                                    new Tuple<String, int>("Kythorn", 30),
+                                                    new Tuple<String, int>("Flamerule", 30),
                                                     new Tuple<String, int>("Midsummer", 1),
                                                     new Tuple<String, int>("Shieldmeet", 0),
-                                                    new Tuple<String, int>("Eleasis", 28),
-                                                    new Tuple<String, int>("Eleint", 28),
+                                                    new Tuple<String, int>("Eleasis", 30),
+                                                    new Tuple<String, int>("Eleint", 30),
                                                     new Tuple<String, int>("Highharvestide", 1),
-                                                    new Tuple<String, int>("Marpenoth", 28),
-                                                    new Tuple<String, int>("Uktar", 28),
+                                                    new Tuple<String, int>("Marpenoth", 30),
+                                                    new Tuple<String, int>("Uktar", 30),
                                                     new Tuple<String, int>("Feast of the Moon", 1),
-                                                    new Tuple<String, int>("Nightal", 28)};
+                                                    new Tuple<String, int>("Nightal", 30)};
             this.dateFormat = "{1} {0}, {2} DR";
         }
 
@@ -262,6 +349,91 @@ namespace DeCamp {
         }
 
         //override add: handle festivals and leap years
+        public override void add(int amount, Interval unit = Interval.second) {
+            if (unit == Interval.time) { return; } // can't add time of day
+            if (unit == Interval.year) {
+                this.year += amount;
+                return;
+            }
+            if ((unit == Interval.month) || (unit == Interval.week)) {
+                Tuple<int, int> d = this.getDate();
+                int month = d.Item1 - 1, date = d.Item2;
+                bool wasFestival = (this.months[month].Item2 <= 1), wasMidsummer = false;
+                if (wasFestival) {
+                    // treat festivals as the fist of the following month
+                    month += 1;
+                    if (this.months[month].Item2 <= 1) {
+                        wasMidsummer = true;
+                        month += 1;
+                    }
+                }
+                if (unit == Interval.week) {
+                    // convert weeks to months, adjusting date by remaining tendays
+                    int extra = amount % 3;
+                    if (extra < 0) {
+                        extra += 3;
+                        amount -= 3;
+                    }
+                    amount /= 3;
+                    if (extra > 0) {
+                        date += extra * 10;
+                        if (wasFestival) {
+                            date -= 1;
+                            wasFestival = false;
+                            wasMidsummer = false;
+                        }
+                    }
+                }
+                // adjust month by amount, adjusting this.year as necessary along the way
+                for (; amount > 0; amount--) {
+                    month += 1;
+                    if (month >= this.months.Length) {
+                        month = 0;
+                        this.year += 1;
+                    }
+                    while (this.months[month].Item2 <= 1) { month += 1; }
+                }
+                for (; amount < 0; amount++) {
+                    month -= 1;
+                    if (month < 0) {
+                        month = this.months.Length - 1;
+                        this.year -= 1;
+                    }
+                    while (this.months[month].Item2 <= 1) { month -= 1; }
+                }
+                // if we started on a festival and can end on one, do so
+                if ((wasFestival) && (month > 0) && (this.months[month - 1].Item2 <= 1)) {
+                    month -= 1;
+                    // if we started on midsummer and can end on it (i.e. amount was a multiple of 12), do so
+                    if ((wasMidsummer) && (month > 0) && (this.months[month - 1].Item2 <= 1)) { month -= 1; }
+                }
+                // turn month and date into day of year...
+                while (month > 0) {
+                    month -= 1;
+                    date += this.months[month].Item2;
+                    if ((this.months[month].Item2 == 0) && ((this.year % 4) == 0)) {
+                        date += 1;
+                    }
+                }
+                // ...then turn day of year into seconds since start of year
+                this.time = (date - 1) * this.getDayLength() + (this.time % this.getDayLength());
+                return;
+            }
+            // we're not falling through from longer spans here, so we won't worry about overflow
+            if (unit <= Interval.day) { amount *= 24; } // convert days to hours
+            if (unit <= Interval.hour) { amount *= 60; } // convert hours to minutes
+            if (unit <= Interval.minute) { amount *= 60; } // convert minutes to seconds
+            this.time += amount;
+            // normalize
+            while (this.time >= this.getYearLength() * this.getDayLength()) {
+                this.time -= this.getYearLength() * this.getDayLength();
+                this.year += 1;
+            }
+            while (this.time < 0) {
+                this.year -= 1;
+                this.time += this.getYearLength() * this.getDayLength();
+            }
+        }
     }
 
 
