cbd7aebc088004c54961243b9b61d27a0fc487a6
manveti <manveti@users.noreply.github.com>
2018-01-25 08:42:14 -0800

    Implement buttons for jumping campaign time forward and backward by common intervals

--- DeCamp/MainWindow.xaml.cs
+++ DeCamp/MainWindow.xaml.cs
@@ -49,17 +49,57 @@ namespace DeCamp {
         }
 
         //other menu handlers
-        //timeline handlers
-/////
-//
+
+        // Timeline handlers
+        public void adjustDate(object sender, RoutedEventArgs e) {
+            int amount = 1;
+            Calendar.Interval unit;
+            String lbl = (String)(((Button)sender).Content);
+            if (lbl.Length != 2) { return; }
+            if ((lbl[0] != '+') && (lbl[0] != '-')) { return; }
+            switch (lbl[1]) {
+            case '?':
+                String[] intervals = new String[] { "year(s)", "month(s)", "week(s)", "day(s)", "hour(s)", "minute(s)", "second(s)" }; // time omitted
+                QueryPrompt[] prompts = {
+                    new QueryPrompt("", QueryType.INT, amount, 0),
+                    new QueryPrompt("", QueryType.LIST, "month(s)", values: intervals)
+                };
+                object[] values = SimpleDialog.askCompound((lbl[0] == '+' ? "Advance" : "Rewind"), prompts, this);
+                if (values == null) { return; }
+                amount = (int)(values[0]);
+                int idx = Array.IndexOf(intervals, values[1]);
+                if ((idx < 0) || (idx >= intervals.Length)) { return; }
+                if (idx >= (int)(Calendar.Interval.time)) { idx += 1; } // adjust idx because time was omitted from intervals
+                unit = (Calendar.Interval)idx;
+                break;
+            case 'Y':
+                unit = Calendar.Interval.year;
+                break;
+            case 'M':
+                unit = Calendar.Interval.month;
+                break;
+            case 'D':
+                unit = Calendar.Interval.day;
+                break;
+            case 'H':
+                unit = Calendar.Interval.hour;
+                break;
+            default:
+                return;
+            }
+            if (lbl[0] == '-') { amount = -amount; }
+            this.campaign.adjustTimestamp(amount, unit);
+            this.showCampaign();
+        }
+
         public void setDate(object sender, RoutedEventArgs e) {
             Timestamp t = Calendars.askTimestamp(this.campaign.calendarName, "Date", this.campaign.getTimestamp(), this);
             if (t == null) { return; }
             this.campaign.setTimestamp(t);
             this.showCampaign();
         }
-//
-/////
+
+        //other timeline handlers
         //party handlers
         //journal handlers
 
